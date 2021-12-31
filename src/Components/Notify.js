553b5bd27c0f8666daae6cf8a8c1b18865f5f4b9
chravi14 <ravindra@haydenfilmsinstitute.org>
2021-12-30 21:38:48 -0600

    Cemboo Alpha Initial Setup

---
+++ src/Components/Notify.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Notify.css";
+
+const Notify = () => {
+  return (
+    <div className="container">
+      <div className="body"></div>
+      {/* <p className="text-content">
+        Cemboo’s mission is to empower content owners with the flexibility to
+        control, monetize and distribute their own digital media content through
+        values of quality, community and innovation.
+      </p> */}
+    </div>
+  );
+};
+
+export default Notify;
